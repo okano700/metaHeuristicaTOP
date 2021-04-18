a6d5d9ae6026201090f892c8ad56d555397e554f
okano700 <okano700@gmail.com>
2021-04-18 18:02:45 -0300

    1804

--- utils/top.py
+++ utils/top.py
@@ -8,7 +8,8 @@ import copy
 import math
 import gc
 import time
-
+from tqdm import tnrange, tqdm_notebook
+import tqdm
 
 
 class top:
@@ -54,6 +55,9 @@ class top:
 
             self.dist = np.array(self.dist)
             self.vDist = np.array(self.vDist)
+            for index,dist in enumerate(self.dist[0]):
+                if dist > self.tmax:
+                    self.nUsed.remove(index)
 
     def add(self, point, car):
         if car < self.m:
@@ -74,6 +78,23 @@ class top:
                 print('Ponto nao atribuido ao carro ')
         else:
             print('Carro inexistente')
+
+    def swap(self, p1, p2, c1, c2):
+        if ((c1> len(self.cars)) or (c2 > len(self.cars)) or (p1 not in self.cars[c1]) or (p2 not in self.cars[c2])):
+            print('Erro de swap')
+        else:
+            i1 = self.cars[c1].index(p1)
+            i2 = self.cars[c2].index(p2)
+            self.cars[c1][i1] = p2
+            self.cars[c2][i2] = p1
+            #print('swap')
+
+    def shift(self, p1,c1,c2):
+        if ((c1> len(self.cars)) or (c2 > len(self.cars)) or (p1 not in self.cars[c1])):
+            print('Erro de shift')
+        else:
+            self.remove(p1,c1)
+            self.add(p1,c2)
 
     def cost(self,car):
         cost = 0
@@ -149,6 +170,134 @@ class top:
         for i in range(self.m):
             teste = 0
         return 0
+
+def shake(S,n):
+    aux = copy.deepcopy(S)
+    for i in range(math.ceil(n)):
+        if (aux.objective_function() < 0):
+            #print(aux.over)
+            for index, over in enumerate(aux.over):
+                if (over > 0):
+                    while(True):
+                        rm = np.random.choice(aux.cars[index])
+                        if (rm != 0) and (rm != (aux.n -1)):
+                            aux.remove(rm,index)
+                            aux.two_opt(index)
+                            aux.objective_function()
+                            break
+        else:
+            car = np.random.choice(aux.m)
+            aux.add(np.random.choice(aux.nUsed), car)
+            aux.two_opt(car)
+            aux.objective_function()
+
+    return aux
+
+def local_search(S, beta = 0.3):
+    aux = copy.deepcopy(S)
+    n = math.ceil(beta * (sum([len(i) for i in aux.cars])))
+    best = copy.deepcopy(aux)
+    """
+    while(any([True if len(i) == 2 else False for i in aux.cars])):
+        for car, i in enumerate(aux.cars):
+            if len(i) == 2:
+                #print(i, car)
+                aux.add(np.random.choice(aux.nUsed), car)
+                aux.two_opt(car)
+                aux.objective_function()
+    """
+    while(any([True if len(i) == 2 else False for i in aux.cars])):
+
+        if (sum([len(j) for j in aux.cars ]) > aux.m*3):
+            for car, i in enumerate(aux.cars):
+                if len(i) == 2:
+                    car2 = [len(j) for j in aux.cars ]
+                    while(True):
+                        c2 = np.argmax(car2)
+                        p2 = np.random.choice(aux.cars[c2])
+                        if ((p2 != 0) and (p2 != (aux.n -1))):
+                            aux.shift(p2,c2,car)
+                            break
+        else:
+            for car, i in enumerate(aux.cars):
+                if len(i) == 2:
+                    #print(i, car)
+                    aux.add(np.random.choice(aux.nUsed), car)
+                    aux.two_opt(car)
+                    aux.objective_function()
+                    break
+
+    for i in range(1,n):
+        car = [i for i in range(aux.m)]
+        c1 = np.random.choice(car)
+        car.remove(c1)
+        c2 = np.random.choice(car)
+        while(True):
+            p1 = np.random.choice(aux.cars[c1])
+            p2 = np.random.choice(aux.cars[c2])
+            if ((p1 != 0) and (p2 != 0) and (p1 != (aux.n -1)) and (p2 != (aux.n -1))):
+                aux.swap(p1,p2,c1,c2)
+                aux.two_opt(c1)
+                aux.two_opt(c2)
+                if aux.objective_function() > best.of:
+                    del best
+                    best = copy.deepcopy(aux)
+                    gc.collect()
+                break
+    return best
+
+def shake2(S,n):
+    aux = copy.deepcopy(S)
+    for i in range(math.ceil(n)):
+        options = ['add', 'remove']
+        escolha = np.random.choice(options)
+        if (escolha == 'add'):
+            car = np.random.choice(aux.m)
+            aux.add(np.random.choice(aux.nUsed), car)
+            aux.two_opt(car)
+            aux.objective_function()
+        elif(escolha == 'remove'):
+            car = np.random.choice(aux.m)
+            while(True):
+                if len(aux.cars[car]) == 2:
+                    break
+                rm = np.random.choice(aux.cars[car])
+                if (rm != 0) and (rm != (aux.n -1)):
+                    aux.remove(rm,car)
+                    aux.two_opt(car)
+                    aux.objective_function()
+                    break
+    return aux
+
+def VNS(S, tmax = 10, alpha = 10, beta = 0.3):
+    aux = copy.deepcopy(S)
+    best = copy.deepcopy(S)
+    start = time.perf_counter()
+    log = []
+    log.append(best.objective_function())
+    now = time.perf_counter()
+    while now < start + tmax:
+        i = 1
+        while (i < alpha):
+            shakes = shake2(aux, i)
+            local = local_search(shakes, beta)
+            #print(local.cars)
+            if local.objective_function() > aux.objective_function():
+                aux = local
+                if local.of > best.of:
+                    best = copy.deepcopy(local)
+                i = 1
+                #print(i)
+            else:
+                i += 1
+            log.append(best.of)
+            #print(aux.cars)
+        now = time.perf_counter()
+        #print(best.of, (now - start))
+    return best, log,(now - start)
+
+
+
 
 def choose_neighbor(S):
     aux = copy.deepcopy(S)
